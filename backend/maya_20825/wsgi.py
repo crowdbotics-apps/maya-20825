dfbb35b92e4ae7f873090dfac95ed6b2c244a543
Crowdbotics <team@crowdbotics.com>
2020-09-29 15:21:42 +0000

    Update models

--- backend/maya_20825/wsgi.py
+++ backend/maya_20825/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'maya_20825.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "maya_20825.settings")
 
 application = get_wsgi_application()
